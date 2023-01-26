0e7cd78b26f26bcd6c7afbba9d3c7ac68bd30c49
haidoe <haidren.amalia@gmail.com>
2023-01-26 09:38:17 -0800

    #proj-40 - Upload Function Added to Helper File

--- src/js/helper.js
+++ src/js/helper.js
@@ -1,3 +1,10 @@
+import { v4 as uuidv4 } from "https://jspm.dev/uuid";
+import {
+  ref,
+  uploadBytesResumable,
+  getDownloadURL,
+} from "https://www.gstatic.com/firebasejs/9.16.0/firebase-storage.js";
+
 //Used to display images
 // convert file to a base64 url
 export const readURL = (file) => {
@@ -7,4 +14,41 @@ export const readURL = (file) => {
     reader.onerror = (e) => rej(e);
     reader.readAsDataURL(file);
   });
+};
+
+export const uploadFile = (
+  storage,
+  file,
+  folderName = "shifts",
+  callbacks = {}
+) => {
+  const fileRef = ref(storage, `${folderName}/${uuidv4()}`);
+
+  const uploadTask = uploadBytesResumable(fileRef, file);
+
+  uploadTask.on(
+    "state_changed",
+    (snapshot) => {
+      const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+
+      if (callbacks.progress) {
+        callbacks.progress(progress);
+      }
+    },
+    (error) => {
+      // Handle unsuccessful uploads
+      if (callbacks.error) {
+        callbacks.error(error);
+      }
+    },
+    () => {
+      getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+        console.log("File available at", downloadURL);
+
+        if (callbacks.getUrl) {
+          callbacks.getUrl(downloadURL);
+        }
+      });
+    }
+  );
 };
--- src/js/upload-image/upload-image.js
+++ src/js/upload-image/upload-image.js
@@ -1,18 +1,18 @@
-import { v4 as uuidv4 } from "https://jspm.dev/uuid";
 import { initialize } from "../../firebase.js";
-import {
-  ref,
-  uploadBytesResumable,
-  getDownloadURL,
-} from "https://www.gstatic.com/firebasejs/9.16.0/firebase-storage.js";
-
+import { readURL, uploadFile } from "../helper.js";
 const { storage } = initialize();
-
 let image;
 
-fileInput.addEventListener("change", (e) => {
+fileInput.addEventListener("change", async (e) => {
   image = e.target.files[0];
+  const url = await readURL(image);
+  imgDisplay.src = url;
 });
+
+//This is to track the progress percentage of the uploaded file
+const progressCallback = (progress) => {
+  console.log("HEYY!!", progress);
+};
 
 formUpload.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -21,23 +21,7 @@ formUpload.addEventListener("submit", (e) => {
 
   console.log(image);
 
-  const imageRef = ref(storage, `shifts/${image.name + uuidv4()}`);
-
-  const uploadTask = uploadBytesResumable(imageRef, image);
-
-  uploadTask.on(
-    "state_changed",
-    (snapshot) => {
-      const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-      console.log("Upload is " + progress + "% done");
-    },
-    (error) => {
-      // Handle unsuccessful uploads
-    },
-    () => {
-      getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
-        console.log("File available at", downloadURL);
-      });
-    }
-  );
+  uploadFile(storage, image, "jobPostingBanner", {
+    progress: progressCallback,
+  });
 });
