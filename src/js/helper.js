45fab7d462dc32fd710ad137cdcb4288a0b6e9df
haidoe <haidren.amalia@gmail.com>
2023-01-26 00:42:17 -0800

    #proj-34 - Added Initial Job Posting Functionality

---
+++ src/js/helper.js
@@ -0,0 +1,10 @@
+//Used to display images
+// convert file to a base64 url
+export const readURL = (file) => {
+  return new Promise((res, rej) => {
+    const reader = new FileReader();
+    reader.onload = (e) => res(e.target.result);
+    reader.onerror = (e) => rej(e);
+    reader.readAsDataURL(file);
+  });
+};
