a34b426b78c869531e803d9f16b29a5b8c652f10
haidoe <haidren.amalia@gmail.com>
2023-01-30 10:52:29 -0800

    #proj-43 - Partially Implemented SPA

--- src/index.js
+++ src/index.js
@@ -6,6 +6,18 @@ import "./css/global.scss";
 
 const { firebaseApp } = initialize();
 
+// For Service Worker
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker
+    .register("./static/service-worker.js")
+    .then(function (reg) {
+      console.log(`Service Worker Registered`);
+    })
+    .catch(function (error) {
+      console.log(`Service Worker Registration Error (${error})`);
+    });
+}
+
 console.log("Instaff Updates");
 
 const body = document.querySelector("body");
