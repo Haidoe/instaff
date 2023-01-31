1cdc1f3cb837643a70c68a67722c7df2505708da
haidoe <haidren.amalia@gmail.com>
2023-01-31 13:58:51 -0800

    Reverted PWA

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,6 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const CopyPlugin = require("copy-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const WorkboxPlugin = require("workbox-webpack-plugin");
 
 const config = (env) => {
   const devtool = env.mode === "development" ? "source-map" : false;
@@ -67,12 +66,6 @@ const config = (env) => {
             to: "static",
           },
         ],
-      }),
-      new WorkboxPlugin.GenerateSW({
-        // these options encourage the ServiceWorkers to get in there fast
-        // and not allow any straggling "old" SWs to hang around
-        clientsClaim: true,
-        skipWaiting: true,
       }),
     ],
   };
