c0431f67f2426c80a0664420513b86abdc3bc4a3
haidoe <haidren.amalia@gmail.com>
2023-02-23 23:43:39 -0800

    Job Post - Mobile related transitions

--- src/pages/JobPostingV2/index.js
+++ src/pages/JobPostingV2/index.js
@@ -2,10 +2,16 @@ import EmployerPage from "../../classes/EmployerPage";
 import template from "./template.html";
 import "./job-post.scss";
 import { readURL } from "../../js/utils";
+import {
+  convertCoordinatesToAddress,
+  convertAddressToCoordinates,
+} from "../../js/map-util";
+import { uploadFile } from "../../js/upload-files/upload-image";
 
 class JobPosting extends EmployerPage {
   constructor() {
     super("Job Posting");
+    //Banner related variables
     this.image = null;
 
     //Map related variables
@@ -13,6 +19,9 @@ class JobPosting extends EmployerPage {
     this.marker = null;
     //This will get the coordinates of the address - on input change
     this.coordinates = null;
+
+    //Form related variables
+    this.jobPosting = {};
   }
 
   async load() {
@@ -65,6 +74,7 @@ class JobPosting extends EmployerPage {
             response.extendedPostalCode;
         }
       } catch (error) {
+        console.log(error);
         console.log("Unable to do autocomplete.");
       }
     });
@@ -79,8 +89,6 @@ class JobPosting extends EmployerPage {
       e.preventDefault();
       this.image = e.target.files[0];
 
-      console.log(this.image);
-
       if (this.image) {
         const imgUrl = await readURL(this.image);
         imageContainer.style.backgroundImage = `url(${imgUrl})`;
@@ -92,9 +100,136 @@ class JobPosting extends EmployerPage {
     });
   }
 
+  formListener() {
+    const partition1 = document.querySelector(".partition-1");
+    const partition2 = document.querySelector(".partition-2");
+    const partition3 = document.querySelector(".partition-3");
+
+    const form1 = document.querySelector("#form1");
+    const form2 = document.querySelector("#form2");
+    const form3 = document.querySelector("#form3");
+
+    const back1 = document.querySelector(".partition-2 .back-btn");
+    const back2 = document.querySelector(".partition-3 .back-btn");
+
+    form1.addEventListener("submit", (e) => {
+      e.preventDefault();
+      const formData = new FormData(form1);
+
+      const data = Object.fromEntries(formData.entries());
+
+      this.jobPosting = {
+        ...this.jobPosting,
+        ...data,
+      };
+
+      partition1.style.display = "none";
+      partition2.style.display = "block";
+    });
+
+    form2.addEventListener("submit", (e) => {
+      e.preventDefault();
+      const formData = new FormData(form2);
+
+      const data = Object.fromEntries(formData.entries());
+
+      this.jobPosting = {
+        ...this.jobPosting,
+        ...data,
+      };
+
+      partition2.style.display = "none";
+      partition3.style.display = "block";
+    });
+
+    form3.addEventListener("submit", (e) => {
+      e.preventDefault();
+      const formData = new FormData(form3);
+
+      const data = Object.fromEntries(formData.entries());
+
+      this.jobPosting = {
+        ...this.jobPosting,
+        ...data,
+      };
+
+      console.log("SUBMIT!!", this.jobPosting);
+    });
+
+    back1.addEventListener("click", (e) => {
+      e.preventDefault();
+      partition1.style.display = "block";
+      partition2.style.display = "none";
+    });
+
+    back2.addEventListener("click", (e) => {
+      e.preventDefault();
+      partition2.style.display = "block";
+      partition3.style.display = "none";
+    });
+  }
+
+  addressListener() {
+    document
+      .getElementById("address")
+      .addEventListener("focusout", async (e) => {
+        e.preventDefault();
+        const city = document.getElementById("city");
+        const address = document.getElementById("address");
+
+        const trimCity = city.value.trim();
+        const trimAddress = address.value.trim();
+
+        if (trimCity && trimAddress) {
+          const query = `${trimAddress}, ${trimCity}, BC, Canada`;
+          const position = await convertAddressToCoordinates(query);
+          this.coordinates = position;
+
+          this.map.easeTo({ center: position });
+          this.marker.setLngLat(position).addTo(this.map);
+        }
+      });
+  }
+
+  async handleSubmit() {
+    // const from = new Date(`${shiftDate.value} ${fromTime.value}`);
+    // const to = new Date(`${shiftDate.value} ${fromTime.value}`);
+    // to.setHours(to.getHours() + Number(hoursOfShift.value));
+
+    const jobPosting = {
+      companyName: companyName.value,
+      positionTitle: positionTitle.value,
+      shiftDate: new Date(shiftDate.value),
+      time: { from, to },
+      wageRate: Number(wage.value),
+      positionAvailable: Number(positionAvailable.value),
+      description: description.value,
+      additionalInfo: additionalInfo.value,
+      city: city.value,
+      address: address.value,
+      userId: this.currentUser.uid,
+      postalCode: postalCode.value,
+      hoursOfShift: Number(hoursOfShift.value),
+    };
+
+    if (this.coordinates) {
+      jobPosting.coordinates = this.coordinates;
+    }
+
+    // jobPosting.bannerImageUrl = await uploadFile(this.image, "jobPostings");
+
+    // const id = await setJobPosting(jobPosting);
+
+    // if (id) {
+    //   pageTransition(`/post/draft/${id}`);
+    // }
+  }
+
   mounted() {
     this.initMap();
     this.imageListener();
+    this.formListener();
+    this.addressListener();
   }
 }
 
