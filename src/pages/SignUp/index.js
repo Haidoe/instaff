f0b1cc29bda8a8cc86d1cde78e307107827c5a5b
JB Sinluenam <jb.sinluenam@gmail.com>
2023-02-24 18:49:03 -0800

    FE-sign up page

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -19,48 +19,51 @@ class SignUp extends Page {
   async load() {
     return `
       <div class="sign-up-page">
-      <div class="left-col">
-      <img src="./static/images/undraw_Work_together.png" alt="work-together" class="undraw-work-together">
-      </div>
-      <div class="right-col">
-            <img src="./static/instaff-logo-light-full-text.svg" alt="instaff logo" class="logo">
-      <form action="#" id="signUpForm" class= "center-form">
-          <div class="container center-form">
-          <h1>Create an account </h1>
-    
+        <div class="inner-wrapper  gradient-bg">
+          <div class="left-col">
+          </div>
+          <div class="right-col">
+                <img src="./static/instaff-logo-light-full-text.svg" alt="instaff logo" class="logo">
+          <form action="#" id="signUpForm" class= "signup-signin-form">
+              <div class="container center-form">
+              <h1>Create an account </h1>
         
-          <div class="group input-group-2cols">
-            <label for="displayname">Name</label>
-            <input type="text" placeholder="Enter Display Name" name="displayname" id="displayname" required>
-          </div>
+            
+              <div class="group input-group-2cols">
+                <label for="displayname">Name</label>
+                <input type="text" placeholder="Enter Display Name" name="displayname" id="displayname" required>
+              </div>
 
-          <div class="group input-group-2cols">
-            <label for="email">Email</label>
-            <input type="text" placeholder="Enter Email" name="email" id="email" required>
-          </div>
+              <div class="group input-group-2cols">
+                <label for="email">Email address</label>
+                <input type="text" placeholder="Enter Email" name="email" id="email" required>
+              </div>
 
-          <div class="group input-group-2cols">
-            <label for="psw">Password</label>
-            <input type="password" name="psw" id="psw" required>
-          </div>
+              <div class="group input-group-2cols">
+                <label for="psw">Password</label>
+                <div class="password-container">
+                <input type="password" name="psw" id="psw" required>
+                <img src="./static/icons/eye.svg" alt="eye" class="eye">
+                </div>
+              </div>
 
-          <div class="group input-group-2cols">
-            <label for="typeOfUser">Purpose</label>
-            <select name="typeOfUser" id="typeOfUser">
-              <option value="employee">I want to find a job.</option>
-              <option value="employer">I want to hire staffs.</option>
-            </select>
-          </div>
+              <div class="group input-group-2cols">
+                <label for="typeOfUser">Purpose</label>
+                <select name="typeOfUser" id="typeOfUser">
+                  <option value="employee">I am looking for job.</option>
+                  <option value="employer">I want to hire staffs.</option>
+                </select>
+              </div>
 
-          <button type="submit" id="submitData" name="submitData" class="button-white lock-bottom">Next</button>
-          </div>
-          <div class="group">
-            <p id="output"></p>
-          </div>
+              <button type="submit" id="submitData" name="submitData" class="button-white lock-bottom">Next</button>
+              </div>
+              <div class="group">
+                <p id="output"></p>
+              </div>
 
-       </form>
-      </div>
-
+          </form>
+          </div>
+        </div>
 
       </div>
     `;
@@ -114,6 +117,20 @@ class SignUp extends Page {
           alert(errorMessage);
         });
     });
+    
+
+    //toggle password visibility
+    const eye = document.querySelector(".eye");
+    const password = document.querySelector("#psw");
+    eye.addEventListener("click", () => {
+      if (password.type === "password") {
+        password.type = "text";
+        eye.src = "./static/icons/eye-slash.svg";
+      } else {
+        password.type = "password";
+        eye.src = "./static/icons/eye.svg";
+      }
+    })
   }
 
   close() {
