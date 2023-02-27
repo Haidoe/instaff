92456017502ec2c84424ad6bfd3fc601f27772b2
JB Sinluenam <jb.sinluenam@gmail.com>
2023-02-27 14:53:37 -0800

    fix letter-spacing - text on botton-sign in-sign up-verification

--- src/pages/Verification/index.js
+++ src/pages/Verification/index.js
@@ -2,6 +2,7 @@ import Page from "../../classes/Page";
 import "./verification.scss";
 import { getAuth, sendEmailVerification } from "firebase/auth";
 import { pageTransition } from "../../router";
+import pubsub from "../../classes/PubSub";
 
 class Verification extends Page {
   constructor() {
@@ -12,22 +13,28 @@ class Verification extends Page {
     // Html here
     // always wrap the whole markup
     return `
-    <div class="verifyemail">
-      <h1>Please verify your email</h1>
-      <div class="group">icon here</div>
-      <div class="group">
-        <p>You're almost there! We sent an email to <span id="useremail"></span></p>
-        <p>Just click on the link in that email to complete your sign up.</p>
-      </div>
-      <div class="group">
-        <p>Still can't find the email?</p>
-      </div>
-
-      
-      <button type="button" id="resendemail">Resend Email</button>
-
-      <div class="group">
-        <p id="output"></p>
+    <div class="verification-page">
+      <div class="inner-wrapper gradient-bg">
+        <div class="left-col">
+        </div>
+        <div class="right-col">
+          <div class="container">
+            <h1>Please verify your email</h1>
+            <img src="./static/images/verify-letter.svg" alt="envelope picture" class="envelope">
+            <div class="group">
+              <p>You're almost there! We sent an email to <span id="useremail"></span></p>
+              <p>Click the link in that email to complete your sign up.</p>
+            </div>
+            <div class="group">
+              <p>Still can't find the email?</p>
+            </div>
+            
+            <button type="button" id="resendemail" class="button-white">Resend Email</button>
+            <div class="group">
+              <p id="output"></p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
     `;
@@ -51,6 +58,8 @@ class Verification extends Page {
   }
 
   async mounted() {
+    document.querySelector("body").classList.add("verification-body");
+    pubsub.publish("hideMainHeader");
     const auth = getAuth();
 
     //grab elements
@@ -63,11 +72,24 @@ class Verification extends Page {
       sendEmailVerification(auth.currentUser).then(() => {
         const output = document.querySelector("#output");
 
+        //loadind
+        btnResend.innerHTML = `<div>Sending...</div>`;
+
+
+
         // Email verification sent!
         output.innerHTML = `<em>Email Verification sent! Check your mail box.</em>`;
         // ...
+
+        btnResend.innerHTML = `<div>Resend Email</div>`;
+
       });
     });
+  }
+
+  close() {
+    document.querySelector("body").classList.remove("verification-body");
+    pubsub.publish("showMainHeader");
   }
 }
 export default Verification;
