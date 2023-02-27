a1d60f923a72d214f386adb4238e431d203f3178
haidoe <haidren.amalia@gmail.com>
2023-02-26 22:46:20 -0800

    Job Posting Detail Modal - Update and Initial Func

--- src/components/modal/job-posting-detail/index.js
+++ src/components/modal/job-posting-detail/index.js
@@ -1,7 +1,9 @@
+import { extractTime } from "../../../js/utils";
 import "./job-posting-detail.scss";
 
 class Modal {
-  constructor(textContent) {
+  constructor(data) {
+    this.data = data;
     //This is where the modal get rendered.
     this.wrapper = document.querySelector("body");
 
@@ -15,8 +17,7 @@ class Modal {
 
     this.modalContent = document.createElement("div");
     this.modalContent.classList.add("modal-content");
-    this.modalContent.textContent =
-      textContent || "Please, replace with me something special.";
+    this.initContent();
     this.modalContainer.appendChild(this.modalContent);
 
     //Close Button Related
@@ -40,6 +41,168 @@ class Modal {
 
     this.modalClose.addEventListener("click", () => {
       this.handleClose();
+    });
+  }
+
+  initContent() {
+    this.modalContentHeader = document.createElement("header");
+    this.modalContent.appendChild(this.modalContentHeader);
+
+    this.modalContentHeaderImage = document.createElement("img");
+    this.modalContentHeaderImage.src = this.data.bannerImageUrl;
+    this.modalContentHeaderImage.alt = "job posting banner";
+    this.modalContentHeader.appendChild(this.modalContentHeaderImage);
+
+    this.modalContentHeaderTitle = document.createElement("h2");
+    this.modalContentHeaderTitle.textContent = this.data.companyName;
+    this.modalContentHeader.appendChild(this.modalContentHeaderTitle);
+
+    this.modalContentMeta = document.createElement("div");
+    this.modalContentMeta.classList.add("modal-meta");
+    this.modalContent.appendChild(this.modalContentMeta);
+
+    this.modalContentMetaButton = document.createElement("button");
+    this.modalContentMetaButton.classList.add("primary-button");
+    this.modalContentMetaButton.textContent = "Apply for this job!";
+    this.modalContentMeta.appendChild(this.modalContentMetaButton);
+
+    this.modalContentBody = document.createElement("div");
+    this.modalContentBody.classList.add("modal-body");
+    this.modalContent.appendChild(this.modalContentBody);
+
+    this.modalContentBodyNav = document.createElement("nav");
+    this.modalContentBody.appendChild(this.modalContentBodyNav);
+
+    this.modalContentBodyNavList = document.createElement("ul");
+    this.modalContentBodyNav.appendChild(this.modalContentBodyNavList);
+
+    this.modalContentBodyNavListDetails = document.createElement("li");
+    this.modalContentBodyNavListDetails.classList.add("active");
+    this.modalContentBodyNavList.appendChild(
+      this.modalContentBodyNavListDetails
+    );
+
+    this.modalContentBodyNavListDetailsLink = document.createElement("a");
+    this.modalContentBodyNavListDetailsLink.href = "javascript:void(0)";
+    this.modalContentBodyNavListDetailsLink.textContent = "Details";
+    this.modalContentBodyNavListDetails.appendChild(
+      this.modalContentBodyNavListDetailsLink
+    );
+
+    this.modalContentBodyNavListRating = document.createElement("li");
+    this.modalContentBodyNavList.appendChild(
+      this.modalContentBodyNavListRating
+    );
+
+    this.modalContentBodyNavListRatingLink = document.createElement("a");
+    this.modalContentBodyNavListRatingLink.href = "javascript:void(0)";
+    this.modalContentBodyNavListRatingLink.textContent = "Rating";
+    this.modalContentBodyNavListRating.appendChild(
+      this.modalContentBodyNavListRatingLink
+    );
+
+    this.initBodySectionDetail();
+    this.initBodySectionRating();
+
+    this.initContentNavListeners();
+  }
+
+  initBodySectionDetail() {
+    this.modalContentBodySectionDetails = document.createElement("section");
+    this.modalContentBodySectionDetails.classList.add("details");
+    this.modalContentBody.appendChild(this.modalContentBodySectionDetails);
+
+    const infoPositionTitle = document.createElement("div");
+    infoPositionTitle.classList.add("info-group");
+    this.modalContentBodySectionDetails.appendChild(infoPositionTitle);
+
+    const infoPositionProp = document.createElement("div");
+    infoPositionProp.classList.add("prop");
+    infoPositionProp.textContent = "Position";
+    infoPositionTitle.appendChild(infoPositionProp);
+
+    const infoPositionValue = document.createElement("div");
+    infoPositionValue.classList.add("value");
+    infoPositionValue.textContent = this.data.positionTitle;
+    infoPositionTitle.appendChild(infoPositionValue);
+
+    const infoSchedule = document.createElement("div");
+    infoSchedule.classList.add("info-group");
+    this.modalContentBodySectionDetails.appendChild(infoSchedule);
+
+    const infoScheduleProp = document.createElement("div");
+    infoScheduleProp.classList.add("prop");
+    infoScheduleProp.textContent = "Schedule";
+    infoSchedule.appendChild(infoScheduleProp);
+
+    const infoScheduleValue = document.createElement("div");
+    infoScheduleValue.classList.add("value");
+    infoSchedule.appendChild(infoScheduleValue);
+
+    const parsedDate = this.data.shiftDate.toDate().toDateString();
+    const infoScheduleValueDate = document.createElement("div");
+    infoScheduleValueDate.textContent = parsedDate;
+    infoScheduleValue.appendChild(infoScheduleValueDate);
+
+    const time = {
+      from: extractTime(this.data.time.from),
+      to: extractTime(this.data.time.to),
+    };
+
+    const infoScheduleValueRange = document.createElement("div");
+    infoScheduleValueRange.textContent = `${time.from} - ${time.to}`;
+    infoScheduleValue.appendChild(infoScheduleValueRange);
+
+    const infoLocation = document.createElement("div");
+    infoLocation.classList.add("info-group");
+    this.modalContentBodySectionDetails.appendChild(infoLocation);
+
+    const infoLocationProp = document.createElement("div");
+    infoLocationProp.classList.add("prop");
+    infoLocationProp.textContent = "Address";
+    infoLocation.appendChild(infoLocationProp);
+
+    const infoLocationValue = document.createElement("div");
+    infoLocationValue.classList.add("value");
+    infoLocationValue.textContent = ` ${this.data.address}, ${this.data.city}, ${this.data.province}, ${this.data.postalCode}`;
+    infoLocation.appendChild(infoLocationValue);
+
+    const infoDescription = document.createElement("div");
+    infoDescription.classList.add("info-group");
+    this.modalContentBodySectionDetails.appendChild(infoDescription);
+
+    const infoDescriptionProp = document.createElement("div");
+    infoDescriptionProp.classList.add("prop");
+    infoDescriptionProp.textContent = "Description";
+    infoDescription.appendChild(infoDescriptionProp);
+
+    const infoDescriptionValue = document.createElement("div");
+    infoDescriptionValue.classList.add("value");
+    infoDescriptionValue.textContent = this.data.description;
+    infoDescription.appendChild(infoDescriptionValue);
+  }
+
+  initBodySectionRating() {
+    this.modalContentBodySectionRating = document.createElement("section");
+    this.modalContentBodySectionRating.classList.add("rating");
+    this.modalContentBodySectionRating.classList.add("hidden");
+    this.modalContentBodySectionRating.textContent = "No Rating Yet.";
+    this.modalContentBody.appendChild(this.modalContentBodySectionRating);
+  }
+
+  initContentNavListeners() {
+    this.modalContentBodyNavListDetailsLink.addEventListener("click", () => {
+      this.modalContentBodyNavListDetails.classList.add("active");
+      this.modalContentBodyNavListRating.classList.remove("active");
+      this.modalContentBodySectionRating.classList.add("hidden");
+      this.modalContentBodySectionDetails.classList.remove("hidden");
+    });
+
+    this.modalContentBodyNavListRatingLink.addEventListener("click", () => {
+      this.modalContentBodyNavListDetails.classList.remove("active");
+      this.modalContentBodyNavListRating.classList.add("active");
+      this.modalContentBodySectionRating.classList.remove("hidden");
+      this.modalContentBodySectionDetails.classList.add("hidden");
     });
   }
 
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,6 +1,7 @@
 import Pages from "../../classes/Page";
 import Modal from "../../components/modal/job-posting-detail";
 import getAllActiveJobPostings from "../../js/job-posting/getAllActiveJobPostings";
+import { extractTime, formatDate } from "../../js/utils";
 import Template from "./home.html";
 import "./home.scss";
 
@@ -37,92 +38,11 @@ class Home extends Pages {
     const response = await getAllActiveJobPostings();
 
     for (let job of response) {
-      // console.log(job, job.coordinates);
-
       const marker = new tt.Marker().setLngLat(job.coordinates).addTo(this.map);
 
       marker.getElement().addEventListener("click", () => {
-        //Open the modal here.
-        const modal = new Modal();
+        const modal = new Modal(job);
         modal.wrapper = document.querySelector(".new-home-page");
-        console.log(modal.wrapper);
-
-        //If this will be massive, create a new html for this.
-        //But it will be easier to maintain if you create a javascript class element for this.
-        //Just like modal ---- check components/modal/index.js
-        modal.modalContent.innerHTML = `
-
-              <header>
-                <img src="${job.bannerImageUrl}" alt="job posting banner" />
-
-                <h2>${job.companyName}</h2>
-              </header>
-
-              <div class="modal-meta">
-                <button class="primary-button">
-                  Apply for this job!
-                </button>
-              </div>
-
-              <div class="modal-body">
-                <nav>
-                  <ul>
-                    <li class="active">
-                      <a href="#">Details</a>
-                    </li>
-
-                    <li> 
-                      <a href="#">Rating</a> 
-                    </li>
-                  </ul>
-                </nav>
-
-                <section className="details">
-                  <div class="info-group">
-                    <div class="prop">
-                      Position:
-                    </div>
-
-                    <div class="value">
-                      ${job.positionTitle}
-                    </div>
-                  </div>
-
-                  <div class="info-group">
-                    <div class="prop">
-                      Address:
-                    </div>
-
-                    <div class="value">
-                      ${job.address}, ${job.city}, ${job.province}, ${job.postalCode}
-                    </div>
-                  </div>
-
-                  <div class="info-group">
-                    <div class="prop">
-                      Description:
-                    </div>
-
-                    <div class="value">
-                      ${job.description}
-                    </div>
-                  </div>
-
-                  <div class="info-group">
-                    <div class="prop">
-                      Schedule:
-                    </div>
-
-                    <div class="value">
-                      ${job.shiftDate}
-                    </div>
-                  </div>
-                </section>
-              </div>
-
-          `;
-
-        console.log("TEST", modal.modalContent);
         modal.open();
       });
     }
