4274341a3bcc57293b17adbbdc441a16cb186e42
haidoe <haidren.amalia@gmail.com>
2023-03-07 19:23:53 -0800

    Dashboard - Hire and Cancel Hiring Implemented

--- src/pages/Dashboard/components/applicant-box.js
+++ src/pages/Dashboard/components/applicant-box.js
@@ -1,4 +1,6 @@
 import StarRating from "../../../components/star-rating";
+import Modal from "../../../components/modal";
+import { hireApplicant, cancelHiredApplicant } from "../../../js/applicants";
 class ApplicantBox {
   constructor(obj) {
     this.data = obj;
@@ -47,24 +49,74 @@ class ApplicantBox {
 
     meta.appendChild(anchorViewProfile);
 
-    const actionBtns = document.createElement("div");
-    actionBtns.className = "action-btns";
+    this.actionBtns = document.createElement("div");
+    this.actionBtns.className = "action-btns";
 
-    const btnRefuse = document.createElement("button");
-    btnRefuse.className = "refuse secondary-button";
-    btnRefuse.textContent = "Refuse";
-    actionBtns.appendChild(btnRefuse);
-
-    const btnHire = document.createElement("button");
-    btnHire.className = "hire primary-button";
-    btnHire.textContent = "Hire";
-    actionBtns.appendChild(btnHire);
-
-    meta.appendChild(actionBtns);
-
+    meta.appendChild(this.actionBtns);
+    this.initActionBtns();
+    this.renderActionBtns();
     content.appendChild(meta);
-
     this.wrapper.appendChild(content);
+  }
+
+  initActionBtns() {
+    this.btnRefuse = document.createElement("button");
+    this.btnRefuse.className = "refuse secondary-button";
+    this.btnRefuse.textContent = "Reject";
+
+    this.btnHire = document.createElement("button");
+    this.btnHire.className = "hire primary-button";
+    this.btnHire.textContent = "Hire";
+
+    this.btnHire.addEventListener("click", () => {
+      const modal = new Modal();
+      modal.wrapper = this.wrapper;
+      modal.modalContent.innerHTML = `
+        In order to proceed, you must agree to hire the candidate as a <a href="javascript:void(0)"> temporary worker </a> and to <a href="javascript:void(0)"> sign a contract </a> of employment.
+      `;
+
+      modal.handleConfirm = () => {
+        hireApplicant(this.data.id);
+        this.data.status = "hired";
+        this.renderActionBtns();
+        modal.close();
+      };
+
+      modal.open();
+    });
+
+    this.btnCancelHire = document.createElement("button");
+    this.btnCancelHire.className = "refuse secondary-button";
+    this.btnCancelHire.textContent = "Hired.";
+
+    this.btnCancelHire.addEventListener("click", () => {
+      const modal = new Modal();
+      modal.wrapper = this.wrapper;
+      modal.modalContent.innerHTML = `
+        Are you sure you want to cancel hiring, <a href="javascript:void(0)"> ${this.data.userDisplayName} </a>?.
+      `;
+
+      modal.handleConfirm = () => {
+        cancelHiredApplicant(this.data.id);
+        this.data.status = "pending";
+        this.renderActionBtns();
+        modal.close();
+      };
+
+      modal.open();
+    });
+  }
+
+  renderActionBtns() {
+    console.log(this.data);
+    this.actionBtns.innerHTML = "";
+
+    if (this.data.status === "hired") {
+      this.actionBtns.appendChild(this.btnCancelHire);
+    } else {
+      this.actionBtns.appendChild(this.btnRefuse);
+      this.actionBtns.appendChild(this.btnHire);
+    }
   }
 
   toElement() {
