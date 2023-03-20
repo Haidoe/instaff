e532841c19894a95427aabd2ace14bad7633e768
arcpex <arcpex@gmail.com>
2023-03-20 12:41:53 -0700

    FisrtTime UI update2

--- src/components/modal/first-time/index.js
+++ src/components/modal/first-time/index.js
@@ -7,14 +7,26 @@ class FirstTime extends Modal {
 
     this.modal.classList.add("first-time-modal");
 
-    this.buttonSecondary.textContent = "Maybe later";
-    this.buttonPrimary.textContent = "Go to profile";
+    this.buttonSecondary.textContent = "Just Browsing";
+    this.buttonPrimary.textContent = "Let's do it";
 
     this.modalContent.innerHTML = "";
 
-    const title = document.createElement("p");
-    title.textContent = "To ensure best experience, please complete your profile.";
+    const title = document.createElement("h1");
+    title.textContent = "Thank you for creating an account with us!";
+    const p1 = document.createElement("p");
+    p1.textContent = "To make the most of our services, we kindly ask you to complete two simple steps:";
+
+    p1.classList.add("bold-p");
+    const p2 = document.createElement("p");
+    p2.textContent = "1. To apply for jobs, please provide proof of your work eligibility."
+    const p3 = document.createElement("p");
+    p3.textContent = "2. Set your profile preferences to that we can match you with the best job opportunities."
+    
     this.modalContent.appendChild(title);
+    this.modalContent.appendChild(p1);
+    this.modalContent.appendChild(p2);
+    this.modalContent.appendChild(p3);
 
     this.handleConfirm = () => {
       console.log("Confirm");
