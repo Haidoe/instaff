3d2f9aac70a405fa05766a3603ee7f2145eb31dd
haidoe <haidren.amalia@gmail.com>
2023-03-23 20:35:50 -0700

    Offline Page - Update

--- src/pages/Offline/index.js
+++ src/pages/Offline/index.js
@@ -1,6 +1,5 @@
 import Page from "../../classes/Page";
 import pubsub from "../../classes/PubSub";
-import template from "./offline.html";
 import "./offline.scss";
 
 class OfflinePage extends Page {
@@ -9,7 +8,33 @@ class OfflinePage extends Page {
   }
 
   load() {
-    return template;
+    return `
+    <div class="offline-page">
+      <section>
+        <header>
+          <h2>
+            <img src="/static/instaff-logo-light-full-text.svg" id="mobile-instaff-logo" alt="Instaff Logo">
+
+            <img src="/static/dark-instaff-logo.svg" id="desktop-instaff-logo" alt="Dark Instaff Logo">
+
+            <span class="visually-hidden">
+              Offline
+            </span>
+          </h2>
+        </header>
+
+        <div class="main-content">
+          <img src="/static/illustrator/access-denied.svg" alt="Access Denied illustrator">
+
+          You are currently offline.
+        </div>
+
+        <p>
+          Looks like you’re not connected to a network. Check your settings and try again.
+        </p>
+      </section>
+    </div>
+    `;
   }
 
   mounted() {
